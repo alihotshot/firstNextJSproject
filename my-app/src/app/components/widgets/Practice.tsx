bb9729a2123be53c605ea590dd88c704dd614ac8
alihaidergithub <vergefuture@gmail.com>
2023-11-15 18:50:40 +0500

    14 Nov
    14 Nov

---
+++ my-app/src/app/components/widgets/Practice.tsx
@@ -0,0 +1,16 @@
+import Wrapper from "@/app/components/shared/Wrapper"
+
+const Practice = () => {
+    return (
+        <Wrapper>
+            <div className=" mt-10">
+                <div className=" text-red-900 shadow-lg">
+                    <p>“Give out what you most want to come back.”</p>
+                    <span>Robin Sharma</span>
+                </div>
+            </div>
+        </Wrapper>
+    )
+}
+
+export default Practice
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Hero from '@/app/components/widgets/Hero'
+import Practice from './components/widgets/Practice'
 
 export default function Home() {
   return (
     <main>
       {/* Hero Section */}
       <Hero />
+      <Practice />
       {/* Core Track Section */}
     </main>
   )
