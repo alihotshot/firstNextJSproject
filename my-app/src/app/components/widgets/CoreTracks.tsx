3f441c51d7ff388eb41ea5a187f5ee1360f40af1
alihaidergithub <vergefuture@gmail.com>
2023-11-16 20:11:44 +0500

    16 Nov 2023
    16 Nov 2023

---
+++ my-app/src/app/components/widgets/CoreTracks.tsx
@@ -0,0 +1,13 @@
+import Wrapper from "@/app/components/shared/Wrapper"
+
+const CoreTracks = () => {
+    return (
+        <section>
+            <Wrapper>
+                <div>CoreTracks</div>
+            </Wrapper>
+        </section>
+    )
+}
+
+export default CoreTracks
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import Hero from '@/app/components/widgets/Hero'
 import Practice from './components/widgets/Practice'
+import CoreTracks from './components/widgets/CoreTracks'
 
 export default function Home() {
   return (
     <main>
       {/* Hero Section */}
       <Hero />
-      <Practice />
+
       {/* Core Track Section */}
+      <CoreTracks />
+      
+      {/* <Practice /> */}
     </main>
   )
 }
