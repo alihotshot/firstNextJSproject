61efe5afd0fb257ab204311929b4d2b3b96ec1e1
alihaidergithub <vergefuture@gmail.com>
2023-11-28 19:56:03 +0500

    28 Nov
    28 Nov

--- my-app/src/app/components/widgets/Boxes.tsx
+++ my-app/src/app/components/widgets/Boxes.tsx
@@ -1,22 +1,36 @@
 
+const CoreTrack = [
+    {
+        header: 'Quarter I',
+        description: 'CS-101: Object-Oriented Programming using Typescript',
+        number: 1
+    },
+    {
+        header: 'Quarter II',
+        description: 'W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform',
+        number: 1
+    },
+    {
+        header: 'Quarter III',
+        description: '$-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development',
+        number: 1
+    }
+]
+
 const Boxes = () => {
     return (
         <div className="flex my-20 space-x-4">
-            <div className=" border rounded-md w-4/12 px-8 py-12 relative">
-                <h4 className=" font-bold text-lg">Quarter I</h4>
-                <p className="mt-2 text-slate-600">CS-101: Object-Oriented Programming using Typescript</p>
-                <span className=" absolute top-4 right-10 text-9xl font-bold text-gray-200 -z-10">1</span>
-            </div>
-            <div className=" border rounded-md w-4/12 px-8 py-12 relative">
-                <h4 className=" font-bold text-lg">Quarter II</h4>
-                <p className="mt-2 text-slate-600">CS-101: Object-Oriented Programming using Typescript</p>
-                <span className=" absolute top-4 right-10 text-9xl font-bold text-gray-200 -z-10">2</span>
-            </div>
-            <div className=" border rounded-md w-4/12 px-8 py-12 relative">
-                <h4 className=" font-bold text-lg">Quarter III</h4>
-                <p className="mt-2 text-slate-600">CS-101: Object-Oriented Programming using Typescript</p>
-                <span className=" absolute top-4 right-10 text-9xl font-bold text-gray-200 -z-10">3</span>
-            </div>
+            {
+                CoreTrack.map((item,i) => {
+                    return (
+                        <div className=" border rounded-md w-4/12 px-8 py-12 relative ">
+                            <h4 className=" font-bold text-lg">{item.header}</h4>
+                            <p className="mt-2 text-slate-600">{item.description}</p>
+                            <span className=" absolute top-4 right-10 text-9xl font-bold text-gray-200 -z-10">{i+1}</span>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
